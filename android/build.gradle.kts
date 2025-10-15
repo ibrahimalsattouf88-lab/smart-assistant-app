165e809532cb4e1e08d2a76692ae2f5c5d2d50a5
ibrahimalsattouf88-lab <ibrahimalsattouf88@gmail.com>
2025-10-16 02:26:11 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,6 @@
-// android/build.gradle.kts  (SMART-ASSISTANT-APP)
-
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("dev.flutter.flutter-plugin-loader") apply false
 }
