17f8171d1ab171d59a3f62e7ec88afb1afa4e1b6
ibrahimalsattouf88-lab <ibrahimalsattouf88@gmail.com>
2025-10-16 02:51:11 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    id("org.jetbrains.kotlin.android") apply false
-    id("dev.flutter.flutter-plugin-loader") apply false
+    id("com.android.application") version "8.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
 }
+
+// لا تضع dev.flutter.flutter-plugin-loader هنا أبداً (مكانه في settings.gradle.kts)
