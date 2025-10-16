63ef9879a75bec77b35e3f0f52affb890462984f
ibrahimalsattouf88-lab <ibrahimalsattouf88@gmail.com>
2025-10-16 03:36:24 +0300

    Delete android/build.gradle.kts

--- android/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-    id("com.android.application") version "8.9.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
-}
-
-// لا تضع dev.flutter.flutter-plugin-loader هنا أبداً (مكانه في settings.gradle.kts)
