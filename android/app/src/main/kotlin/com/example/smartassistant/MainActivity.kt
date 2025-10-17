52b9a8d6b735aefde89ef7ec1c43b147fb784617
ibrahimalsattouf88-lab <ibrahimalsattouf88@gmail.com>
2025-10-17 13:42:23 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/smartassistant/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/smartassistant/MainActivity.kt
@@ -1,5 +1,28 @@
-package com.example.smartassistant
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.example.smartassistant">  <!-- عدّل للباكيج الصحيح في لوحة التحكم -->
 
-import io.flutter.embedding.android.FlutterActivity
+    <application
+        android:label="Smart Assistant"     <!-- عدّل الاسم للتطبيق الآخر -->
+        android:icon="@mipmap/ic_launcher">
+        <activity
+            android:name=".MainActivity"
+            android:exported="true"
+            android:launchMode="singleTop"
+            android:theme="@style/LaunchTheme"
+            android:windowSoftInputMode="adjustResize">
+            <intent-filter>
+                <action android:name="android.intent.action.MAIN"/>
+                <category android:name="android.intent.category.LAUNCHER"/>
+            </intent-filter>
 
-class MainActivity : FlutterActivity()
+            <meta-data
+                android:name="io.flutter.embedding.android.NormalTheme"
+                android:resource="@style/NormalTheme" />
+        </activity>
+
+        <!-- إشارة صريحة لاستخدام v2 -->
+        <meta-data
+            android:name="flutterEmbedding"
+            android:value="2"/>
+    </application>
+</manifest>
